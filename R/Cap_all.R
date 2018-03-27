dbd70fdb62d0a5ecca9b48ec17a94123372a406c
rogilmore <thatrickgilmore@gmail.com>
2018-03-27 08:23:51 -0400

    Incorporate new workflows from Adv Bd prep

---
+++ R/Cap_all.R
@@ -0,0 +1,38 @@
+Cap_all <- function(s, before.hyphen = TRUE) {
+  # Capitalizes i) initial words, ii) words separated by a space or hypen
+  # 
+  # Parameters:
+  # s: character array
+  # before.hyphen: Logical value determining whether to capitalize
+  #   words separated by a hyphen, default is TRUE
+  #
+  # Returns:
+  #   Capitalized character array
+  
+  require(stringr)
+  
+  stopifnot(is.character(s))
+  
+  # Initial cap
+  s <- paste0(toupper(str_sub(s,1,1)), 
+              str_sub(s,2,str_length(s)))
+  
+  # Cap before space
+  space.start <- str_locate(s, " [a-z]?")
+  if (!(is.null(space.start)) && !(is.na(space.start))) {
+    s <- paste0(str_sub(s, 1, space.start[1]),
+                toupper(str_sub(s, space.start[2], space.start[2])),
+                str_sub(s, space.start[2]+1, str_length(s)))
+  }
+  
+  # Cap before hyphen, like miami-dade
+  if (before.hyphen) {
+    hyphen.start <- str_locate(s, "\\-[a-z]?")
+    if (!(is.null(hyphen.start)) && !(is.na(hyphen.start))) {
+      s <- paste0(str_sub(s,1, hyphen.start[1]),
+                  toupper(str_sub(s, hyphen.start[2], hyphen.start[2])),
+                  str_sub(s,hyphen.start[2]+1, str_length(s)))
+    }
+  }
+  s
+}
---
+++ R/render_all_demo.R
@@ -0,0 +1,5 @@
+# renders all *.Rmd demo files in default format
+
+rmarkdown::render("educational_attainment.Rmd")
+rmarkdown::render("spanish_speaking.Rmd")
+rmarkdown::render("site_demographics.Rmd")
